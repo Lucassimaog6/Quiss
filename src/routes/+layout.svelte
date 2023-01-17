ca34855f79ed1035be6d21eca85f345de78aecdb
lucas <22101918@aluno.cotemig.com.br>
2023-01-17 12:53:32 -0300

    Add theme options

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,12 @@
+<script>
+	import { onMount } from 'svelte'
+	import { themeChange } from 'theme-change'
+
+	onMount(() => {
+		themeChange(false)
+	})
+</script>
+
 <style>
     @tailwind base;
     @tailwind components;
@@ -5,7 +14,40 @@
 </style>
 
 <div class="navbar bg-base-100">
-	<div class='navbar-start'></div>
+	<div class='navbar-start'>
+		<select data-choose-theme class="select max-w-xs">
+			<option disabled selected>Escolha seu tema</option>
+			<option value="acid">Acid</option>
+			<option value="aqua">Aqua</option>
+			<option value="autumn">Autumn</option>
+			<option value="black">Black</option>
+			<option value="bumblebee">Bumblebee</option>
+			<option value="business">Business</option>
+			<option value="cmyk">CMYK</option>
+			<option value="coffee">Coffee</option>
+			<option value="corporate">Corporate</option>
+			<option value="cupcake">Cupcake</option>
+			<option value="cyberpunk">Cyberpunk</option>
+			<option value="dark">Dark</option>
+			<option value="dracula">Dracula</option>
+			<option value="emerald">Emerald</option>
+			<option value="fantasy">Fantasy</option>
+			<option value="forest">Forest</option>
+			<option value="garden">Garden</option>
+			<option value="halloween">Halloween</option>
+			<option value="lemonade">Lemonade</option>
+			<option value="light">Light</option>
+			<option value="lofi">Lofi</option>
+			<option value="luxury">Luxury</option>
+			<option value="night">Night</option>
+			<option value="pastel">Pastel</option>
+			<option value="retro">Retro</option>
+			<option value="synthwave">Synthwave</option>
+			<option value="valentine">Valentine</option>
+			<option value="winter">Winter</option>
+			<option value="wireframe">Wireframe</option>
+		</select>
+	</div>
 	<div class='navbar-center'>
 		<a class="btn btn-ghost normal-case text-xl" href='/'>Quiss</a>
 	</div>
@@ -14,7 +56,7 @@
 	</div>
 </div>
 
-<main class='p-10 flex flex-wrap justify-center items-center gap-8'>
+<main class='min-h-screen p-10 flex flex-wrap justify-center items-center gap-8'>
 	<slot />
 </main>
 
